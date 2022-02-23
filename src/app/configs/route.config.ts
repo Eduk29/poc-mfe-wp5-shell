f44a9d8c380b57252c27b0b50c12ba082deb4fe1
José Eduardo Trindade E Marques <edu.temarques@gmail.com>
2022-02-23 19:39:32 -0300

    feat: Integrated react app to shell

--- src/app/configs/route.config.ts
+++ src/app/configs/route.config.ts
@@ -7,6 +7,7 @@ export const appRoutes: Routes = [
   { matcher: startsWith('dashboard'), component: WrapperComponent, data: { importName: 'dashboard', elementName: 'dashboard-element' } },
   { matcher: startsWith('product'), component: WrapperComponent, data: { importName: 'product', elementName: 'product-element' } },
   { matcher: startsWith('user'), component: WrapperComponent, data: { importName: 'user', elementName: 'user-element' } },
+  { matcher: startsWith('payment'), component: WrapperComponent, data: { importName: 'payment', elementName: 'payment-element' } },
   { path: 'home', loadChildren: () => import('../home/home.module').then(m => m.HomeModule) },
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
