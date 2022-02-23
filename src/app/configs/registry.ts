faab9ddffcd412ec7b8716c5becd8f203b1787ff
José Eduardo Trindade E Marques <edu.temarques@gmail.com>
2022-02-23 17:38:44 -0300

    feat: Add User to Shell

--- src/app/configs/registry.ts
+++ src/app/configs/registry.ts
@@ -1,14 +1,19 @@
 import { loadRemoteModule } from '@angular-architects/module-federation';
 
 export const registry = {
-    dashboard: () => loadRemoteModule({
-        type: 'module',
-        remoteEntry: 'http://localhost:3001/remoteEntry.js',
-        exposedModule: './web-components'
-    }),
-    product: () => loadRemoteModule({
-      type: 'module',
-      remoteEntry: 'http://localhost:3002/remoteEntry.js',
-      exposedModule: './web-components'
+  dashboard: () => loadRemoteModule({
+    type: 'module',
+    remoteEntry: 'http://localhost:3001/remoteEntry.js',
+    exposedModule: './web-components'
+  }),
+  product: () => loadRemoteModule({
+    type: 'module',
+    remoteEntry: 'http://localhost:3002/remoteEntry.js',
+    exposedModule: './web-components'
+  }),
+  user: () => loadRemoteModule({
+    type: 'module',
+    remoteEntry: 'http://localhost:3003/remoteEntry.js',
+    exposedModule: './web-components'
   })
 };
